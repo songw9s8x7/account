2e0ace2410c6a5a8efa8206d7d633af10f92df8f
frank.wang <frank.wang@zhntd.com>
2014-10-18 17:39:37 +0800

    add ui

--- gen/com/example/account/R.java
+++ gen/com/example/account/R.java
@@ -16,6 +16,10 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f060000;
         public static final int button2=0x7f060001;
+        public static final int button3=0x7f060004;
+        public static final int editText1=0x7f060003;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060005;
     }
     public static final class layout {
         public static final int account=0x7f030000;
